f33843ae370e4452cba9fb693d144d50604f9059
Lucas Taylor <lucas@perfectlunacy.com>
2021-02-01 21:25:12 -0800

    Adds Room DB models

---
+++ app/src/main/java/com/perfectlunacy/bailiwick/models/db/Post.kt
@@ -0,0 +1,60 @@
+package com.perfectlunacy.bailiwick.models.db
+
+import androidx.room.*
+
+@Entity
+data class Post (
+    @PrimaryKey val id: Int,
+    @ColumnInfo val user_id: Int,
+    @ColumnInfo val timestamp: Long,
+    @ColumnInfo val text: String,
+    @ColumnInfo var signature: String?=null
+)
+
+@Entity(foreignKeys = arrayOf(
+    ForeignKey(entity = Post::class,
+        parentColumns = arrayOf("id"),
+        childColumns = arrayOf("post_id"))))
+data class PostFile(
+    @PrimaryKey val id: Int,
+    @ColumnInfo val post_id: Int,
+    @ColumnInfo val mimeType: String,
+    @ColumnInfo val cid: String,
+    @ColumnInfo val signature: String
+)
+
+@Dao
+interface PostDao {
+    @Insert
+    fun insert(post: Post)
+
+    @Update
+    fun update(post: Post)
+
+    @Delete
+    fun delete(post: Post)
+
+    @Query("SELECT * FROM post")
+    fun all(): List<Post>
+
+    @Query("SELECT * FROM post WHERE user_id=:user_id")
+    fun postsForUser(user_id: Int)
+}
+
+@Dao
+interface PostFileDao {
+    @Insert
+    fun insert(post: PostFile)
+
+    @Update
+    fun update(post: PostFile)
+
+    @Delete
+    fun delete(post: PostFile)
+
+    @Query("SELECT * FROM postfile")
+    fun all(): List<PostFile>
+
+    @Query("SELECT * FROM postfile WHERE post_id=:post_id")
+    fun filesForPost(post_id: Int)
+}
---
+++ app/src/main/java/com/perfectlunacy/bailiwick/models/db/User.kt
@@ -0,0 +1,27 @@
+package com.perfectlunacy.bailiwick.models.db
+
+import androidx.room.*
+
+@Entity
+data class User (
+    @PrimaryKey val id: Int,
+    @ColumnInfo val uid: String,
+    @ColumnInfo val name: String
+)
+
+// TODO: Link an "Identity" to a User, where Identity stores the biographical info that we know
+
+@Dao
+interface UserDao {
+    @Insert
+    fun insert(user: User)
+
+    @Update
+    fun update(user: User)
+
+    @Delete
+    fun delete(user: User)
+
+    @Query("SELECT * FROM user")
+    fun all(): List<User>
+}
